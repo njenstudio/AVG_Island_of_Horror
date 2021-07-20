0e1d8c4b5cd24dc7abd3ac85b7b33f1d4cc44422
KillFoot <ts00776614@gmail.com>
2021-07-20 19:06:03 +0800

    update Title Animator.

---
+++ Assets/Scripts/Animation/AnimationEvents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationEvents : MonoBehaviour
+{
+    public List<Events> events = new List<Events>();
+    public void CallEvent(int index)
+    {
+        if (events.Count > index)
+            events[index].m_event.Invoke();
+    }
+}
+[System.Serializable]
+public class Events
+{
+    public UnityEvent m_event;
+}
---
+++ Assets/Scripts/MainModule/TitleManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    public Animator LogoTitleAnimator;
+    public Animator BGAnimator;
+    public Animator MenuAnimator;
+    bool isOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LogoTitleAnimator.SetTrigger("In");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !isOpen)
+        {
+            LogoTitleAnimator.SetTrigger("Out");
+            BGAnimator.SetTrigger("Out");
+            MenuAnimator.SetTrigger("In");
+            isOpen = true;
+        }
+        else if (Input.GetMouseButtonDown(0) && isOpen)
+        {
+            LogoTitleAnimator.SetTrigger("In");
+            BGAnimator.SetTrigger("In");
+            MenuAnimator.SetTrigger("Out");
+            isOpen = false;
+        }
+    }
+}
